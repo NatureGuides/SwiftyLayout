c6c4fb07ae3e8a1c9a41de0edc06ff3e301c4d7e
Joe Thomson <joseph_2166@mac.com>
2024-03-27 13:10:21 +0000

    Added doNotActivate and extra fillToSuperview options

--- SwiftyLayout/AutolayoutTarget+Constraints.swift
+++ SwiftyLayout/AutolayoutTarget+Constraints.swift
@@ -12,41 +12,47 @@ extension AutolayoutTarget
 {
     /// Constrains this view to another using the given constraints.
     @discardableResult
-    fileprivate func constrain(to other: AutolayoutTarget, priority: UILayoutPriority = .required, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
+    fileprivate func constrain(to other: AutolayoutTarget, priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
     {
         let final = constraints.flatMap{ $0.constraints(between: self, and: other) }
-        self.activateConstraints(final, priority: priority)
+        if doNotActivate == false
+        {
+            self.activateConstraints(final, priority: priority)
+        }
         return final
     }
     
     /// Constrains this view to another using the given constraints.
     @discardableResult
-    public func constrain(to other: AutolayoutTarget, priority: UILayoutPriority = .required, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
+    public func constrain(to other: AutolayoutTarget, priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
     {
-        return self.constrain(to: other, priority: priority, constraints)
+        return self.constrain(to: other, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to fill another.
     @discardableResult
-    public func fill(to other: AutolayoutTarget, priority: UILayoutPriority = .required, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
+    public func fill(to other: AutolayoutTarget, priority: UILayoutPriority = .required, doNotActivate: Bool = false, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
     {
-        return self.constrain(to: other, priority: priority, .fill(insets: insets))
+        return self.constrain(to: other, priority: priority, doNotActivate: doNotActivate, .fill(insets: insets))
     }
     
     /// Constrains this view using the given constraints.
     @discardableResult
-    fileprivate func constrain(priority: UILayoutPriority = .required, _ constraints: [UnaryConstraint]) -> [NSLayoutConstraint]
+    fileprivate func constrain(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: [UnaryConstraint]) -> [NSLayoutConstraint]
     {
         let final = constraints.flatMap{ $0.constraint(for: self) }
-        self.activateConstraints(final, priority: priority)
+        if doNotActivate == false
+        {
+            self.activateConstraints(final, priority: priority)
+        }
         return final
     }
     
     /// Constrains this view using the given constraints.
     @discardableResult
-    public func constrain(priority: UILayoutPriority = .required, _ constraints: UnaryConstraint...) -> [NSLayoutConstraint]
+    public func constrain(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: UnaryConstraint...) -> [NSLayoutConstraint]
     {
-        return self.constrain(priority: priority, constraints)
+        return self.constrain(priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Activates the given constraints and sets `translatesAutoresizingMaskIntoConstraints` to `false` if possible.
@@ -68,100 +74,133 @@ extension AutolayoutTarget
 {
     /// Constrains this view to its superview using the given constraints.
     @discardableResult
-    public func constrainToSuperview(priority: UILayoutPriority = .required, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
+    public func constrainToSuperview(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview, priority: priority, constraints)
+        return self.constrain(to: superview, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to its superview using the given constraints.
     @discardableResult
-    public func constrainToSuperview(priority: UILayoutPriority = .required, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
+    public func constrainToSuperview(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview, priority: priority, constraints)
+        return self.constrain(to: superview, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to fill its superview.
     @discardableResult
-    public func fillToSuperview(priority: UILayoutPriority = .required, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
+    public func fillToSuperview(priority: UILayoutPriority = .required, doNotActivate: Bool = false, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview, priority: priority, .fill(insets: insets))
+        return self.constrain(to: superview, priority: priority, doNotActivate: doNotActivate, .fill(insets: insets))
+    }
+    
+    /// Constrains this view to fill its superview.
+    @discardableResult
+    public func fillToSuperview(priority: UILayoutPriority = .required, doNotActivate: Bool = false, inset: CGFloat) -> [NSLayoutConstraint]
+    {
+        guard let superview = self.superview else { return [] }
+        return self.constrain(to: superview, priority: priority, doNotActivate: doNotActivate, .fill(inset: inset))
     }
     
     /// Constrains this view to its superview's layout margins using the given constraints.
     @discardableResult
-    public func constrainToSuperviewLayoutMargins(priority: UILayoutPriority = .required, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
+    public func constrainToSuperviewLayoutMargins(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, constraints)
+        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to its superview's layout margins using the given constraints.
     @discardableResult
-    public func constrainToSuperviewLayoutMargins(priority: UILayoutPriority = .required, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
+    public func constrainToSuperviewLayoutMargins(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, constraints)
+        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to fill its superview's layout margins.
     @discardableResult
-    public func fillToSuperviewLayoutMargins(priority: UILayoutPriority = .required, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
+    public func fillToSuperviewLayoutMargins(priority: UILayoutPriority = .required, doNotActivate: Bool = false, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, .fill(insets: insets))
+        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, doNotActivate: doNotActivate, .fill(insets: insets))
+    }
+    
+    /// Constrains this view to fill its superview's layout margins.
+    @discardableResult
+    public func fillToSuperviewLayoutMargins(priority: UILayoutPriority = .required, doNotActivate: Bool = false, inset: CGFloat) -> [NSLayoutConstraint]
+    {
+        guard let superview = self.superview else { return [] }
+        return self.constrain(to: superview.layoutMarginsGuide, priority: priority, doNotActivate: doNotActivate, .fill(inset: inset))
     }
     
     /// Constrains this view to its superview's safe area using the given constraints.
     @available(iOS 11.0, *)
     @discardableResult
-    public func constrainToSuperviewSafeArea(priority: UILayoutPriority = .required, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
+    public func constrainToSuperviewSafeArea(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, constraints)
+        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to its superview's safe area using the given constraints.
     @available(iOS 11.0, *)
     @discardableResult
-    public func constrainToSuperviewSafeArea(priority: UILayoutPriority = .required, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
+    public func constrainToSuperviewSafeArea(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, constraints)
+        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to fill its superview's safe area.
     @available(iOS 11.0, *)
     @discardableResult
-    public func fillToSuperviewSafeArea(priority: UILayoutPriority = .required, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
+    public func fillToSuperviewSafeArea(priority: UILayoutPriority = .required, doNotActivate: Bool = false, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, .fill(insets: insets))
+        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, doNotActivate: doNotActivate, .fill(insets: insets))
+    }
+    
+    /// Constrains this view to fill its superview's safe area.
+    @available(iOS 11.0, *)
+    @discardableResult
+    public func fillToSuperviewSafeArea(priority: UILayoutPriority = .required, doNotActivate: Bool = false, inset: CGFloat) -> [NSLayoutConstraint]
+    {
+        guard let superview = self.superview else { return [] }
+        return self.constrain(to: superview.safeAreaLayoutGuide, priority: priority, doNotActivate: doNotActivate, .fill(inset: inset))
     }
     
     /// Constrains this view to its superview's readable content guide using the given constraints.
     @discardableResult
-    public func constrainToSuperviewReadableGuide(priority: UILayoutPriority = .required, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
+    public func constrainToSuperviewReadableGuide(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: [BinaryConstraint]) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.readableContentGuide, priority: priority, constraints)
+        return self.constrain(to: superview.readableContentGuide, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to its superview's readable content guide using the given constraints.
     @discardableResult
-    public func constrainToSuperviewReadableGuide(priority: UILayoutPriority = .required, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
+    public func constrainToSuperviewReadableGuide(priority: UILayoutPriority = .required, doNotActivate: Bool = false, _ constraints: BinaryConstraint...) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.readableContentGuide, priority: priority, constraints)
+        return self.constrain(to: superview.readableContentGuide, priority: priority, doNotActivate: doNotActivate, constraints)
     }
     
     /// Constrains this view to fill its superview's readable content guide.
     @discardableResult
-    public func fillToSuperviewReadableGuide(priority: UILayoutPriority = .required, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
+    public func fillToSuperviewReadableGuide(priority: UILayoutPriority = .required, doNotActivate: Bool = false, insets: UIEdgeInsets = .zero) -> [NSLayoutConstraint]
     {
         guard let superview = self.superview else { return [] }
-        return self.constrain(to: superview.readableContentGuide, priority: priority, .fill(insets: insets))
+        return self.constrain(to: superview.readableContentGuide, priority: priority, doNotActivate: doNotActivate, .fill(insets: insets))
+    }
+    
+    /// Constrains this view to fill its superview's readable content guide.
+    @discardableResult
+    public func fillToSuperviewReadableGuide(priority: UILayoutPriority = .required, doNotActivate: Bool = false, inset: CGFloat) -> [NSLayoutConstraint]
+    {
+        guard let superview = self.superview else { return [] }
+        return self.constrain(to: superview.readableContentGuide, priority: priority, doNotActivate: doNotActivate, .fill(inset: inset))
     }
 }
